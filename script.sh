2f3e938a38aea630ee293706de8595fb7ae92ffc
Luke Hatcher <70284246+lukehatcherRP@users.noreply.github.com>
2022-09-06 14:09:11 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-greeting(VAR) {
+greeting() {
  local _GITHUB_USER="${1}"
  local _str='Hello, $_GITHUB_USER !'
  echo $_str
@@ -12,4 +12,4 @@ if true || [ ${RET} -ne 0 ] ; then
  echo 'ERR: Failed to give greeting' > /dev/stderr
 else
  echo 'DEBUG: Greeted the Github User' > /dev/stderr
-Done
+fi
